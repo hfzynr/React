c19c6ecc36d2baebe915c686949e6c7b63e27972
Hafiz <hafizyanuaribrahim@next-ti.co.id>
2022-07-13 09:07:08 +0700

    Props

--- Tutorial Prawito/react-hello-world/src/component/YoutubeComp/YoutubeComp.jsx
+++ Tutorial Prawito/react-hello-world/src/component/YoutubeComp/YoutubeComp.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './YoutubeComp.css';
 
-const YoutubeComp = () => {
+const YoutubeComp = (props) => {
     return (
         <div className="youtube-wrapper">
             <div className="img-thumb">
-                <img src="https://images6.alphacoders.com/124/1242360.jpg" alt=""/>
-                <p className="time">7.12</p>
+                <img src={props.img} alt=""/>
+                <p className="time">{props.time}</p>
             </div>
-            <p className="title">title here</p>
-            <p className="desc">desc here</p>
+            <p className="title">{props.title}</p>
+            <p className="desc">{props.desc}</p>
         </div>
     )
+}
+
+YoutubeComp.defaultProps = {
+    time: '0.00',
+    title: 'Title Here',
+    img: 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150608-27674-obj7u0_7c60f729.jpeg?region=0%2C0%2C1200%2C513',
+    desc: 'Desc Here'
 }
 
 export default YoutubeComp;
--- Tutorial Prawito/react-hello-world/src/container/Home/Home.jsx
+++ Tutorial Prawito/react-hello-world/src/container/Home/Home.jsx
@@ -7,11 +7,32 @@ class Home extends Component {
             <div>
                 <p>Youtube Component</p>
                 <hr/>
-                <YoutubeComp/>
-                <YoutubeComp/>
-                <YoutubeComp/>
-                <YoutubeComp/>
-                <YoutubeComp/>
+                <YoutubeComp 
+                    img="https://images6.alphacoders.com/124/1242360.jpg" 
+                    time="7.12"  
+                    title="Grogu" 
+                    desc="Ini Grogu"/>
+                <YoutubeComp 
+                    img="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/05/30/2855500915.jpg" 
+                    time="8.59"  
+                    title="Anakin"
+                    desc="Ini Anakin"/>
+                <YoutubeComp 
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCe5Vo3l62eErCKtXQ5O1t1sQuN_NRDEtAHmNaOQ2DKij4nsMNvkmJ1thV7sc93oxLhbk&usqp=CAU" 
+                    time="2.15"  
+                    title="Luke"
+                    desc="Ini Luke"/>
+                <YoutubeComp 
+                    img="https://cdn.mos.cms.futurecdn.net/qBCRauHYvdMYcjGgvEnDVH.jpg" 
+                    time="4.17"  
+                    title="Padme"
+                    desc="Ini Padme"/>
+                <YoutubeComp 
+                    img="https://img1.looper.com/img/gallery/the-mandalorian-how-rich-is-din-djarin/intro-1605382117.jpg" 
+                    time="10.12" 
+                    title="Djin Djarin"
+                    desc="Ini Djin Jarin"/>
+                <YoutubeComp />
             </div>
         )
     }
